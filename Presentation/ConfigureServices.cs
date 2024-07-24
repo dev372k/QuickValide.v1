6de9c8980b7b07c1c1321ac8e6d229dcb2c33687
S.M.Owais <smowais.neduet@gmail.com>
2024-07-24 17:19:57 +0500

    change API layer to Presentation layer

---
+++ Presentation/ConfigureServices.cs
@@ -0,0 +1,41 @@
+﻿namespace ApiService;
+
+public static class ConfigureServices
+{
+    public static void ServicesRegistry(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Repositories(configuration);
+        services.Services(configuration);
+        services.Database(configuration);
+        services.Misc(configuration);
+    }
+
+    public static void Repositories(this IServiceCollection services, IConfiguration configuration)
+    {
+
+    }
+
+    public static void Misc(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(opt =>
+        {
+            opt.AddPolicy(name: "_policy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin();
+            });
+        });
+    }
+
+    public static void Services(this IServiceCollection services, IConfiguration configuration)
+    {
+
+    }
+
+    public static void Database(this IServiceCollection services, IConfiguration configuration)
+    {
+        //services.AddDbContextPool<ApplicationDBContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("cs")));
+    }
+}
