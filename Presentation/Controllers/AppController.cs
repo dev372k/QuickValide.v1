f39d9b68309cdc194d1bd9ddb634accd15541346
Syed Inshal Waris <s.inshal12@gmail.com>
2024-07-26 10:28:16 +0500

    App Module

--- Presentation/Controllers/AppController.cs
+++ Presentation/Controllers/AppController.cs
@@ -1,15 +1,32 @@
-﻿using Domain.Repositories.Services;
+﻿using Domain.IRepositories;
 using Microsoft.AspNetCore.Mvc;
 using Shared.Commons;
+using Shared.DTOs.AppDTOs;
 using Shared.Extensions;
 
 namespace Presentation.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class AppController(IGPTService gPTService) : ControllerBase
+public class AppController(IAppRepo _appRepo) : ControllerBase
 {
-    [HttpGet("gpt")]
-    public async Task<IActionResult> Get(string input)
-    => Ok(await gPTService.ChatCompletion(input).ToResponseAsync(message: ResponseMessages.USER_ADDED));
+    [HttpPost("Add")]
+    public async Task<IActionResult> Add(AddAppDTO request)
+        => Ok(await _appRepo.AddAsync(request).ToResponseAsync(message: ResponseMessages.APP_ADDED));
+
+    [HttpPost("Update")]
+    public async Task<IActionResult> Update(UpdateAddAppDTO request)
+       => Ok(await _appRepo.UpdateAsync(request).ToResponseAsync(message: ResponseMessages.APP_UPDATE));
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+        => Ok(await _appRepo.GetAllAsync().ToResponseAsync());
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+        => Ok(await _appRepo.GetByIdAsync(id).ToResponseAsync());
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+        => Ok(await _appRepo.DeleteAsync(id).ToResponseAsync(message: ResponseMessages.USER_DELETED));
 }
--- Shared/Exceptions/Messages/ExceptionMessages.cs
+++ Shared/Exceptions/Messages/ExceptionMessages.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shared.Exceptions.Messages
+﻿namespace Shared.Exceptions.Messages
 {
     public class ExceptionMessages
     {
         public const string USER_DOESNOT_EXIST = "User does not exist.";
         public const string USER_ALREADY_EXIST = "User already exists.";
         public const string INVALID_CREDENTIALS = "Invalid credentials.";
-
+        public const string Domain_ALREADY_EXIST = "Domain already exists.";
+        public const string APP_DOESNOT_EXIST = "App does not exist.";
+        public const string RECORD_NOT_FOUND = "Record not found";
+        public const string FAILED = "Failed";
 
         public const string BLOG_DOESNOT_EXIST = "Blog does not exist.";
 
