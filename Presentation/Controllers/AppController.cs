d86c631e486cf1101e21b7d5644e145fdd1e31e0
S.M.Owais <smowais.neduet@gmail.com>
2024-07-29 16:22:35 +0500

    Update AppController.cs

--- Presentation/Controllers/AppController.cs
+++ Presentation/Controllers/AppController.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.IRepositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shared.Commons;
 using Shared.DTOs.AppDTOs;
@@ -10,11 +11,13 @@ namespace Presentation.Controllers;
 [ApiController]
 public class AppController(IAppRepo _appRepo) : ControllerBase
 {
-    [HttpPost]
+    [HttpPost, Authorize]
+    [IsAuthorized(["Admin", "User"])]
     public async Task<IActionResult> Post(AddAppDTO request)
         => Ok(await _appRepo.AddAsync(request).ToResponseAsync(message: ResponseMessages.APP_ADDED));
 
-    [HttpPut]
+    [HttpPut, Authorize]
+    [IsAuthorized(["Admin", "User"])]
     public async Task<IActionResult> Put(UpdateAddAppDTO request)
        => Ok(await _appRepo.UpdateAsync(request).ToResponseAsync(message: ResponseMessages.APP_UPDATED));
 
@@ -22,11 +25,13 @@ public class AppController(IAppRepo _appRepo) : ControllerBase
     //public async Task<IActionResult> Get()
     //    => Ok(await _appRepo.GetAllAsync().ToResponseAsync());
 
-    [HttpGet("{id}")]
+    [HttpGet("{id}"), Authorize]
+    [IsAuthorized(["Admin", "User"])]
     public async Task<IActionResult> Get(int id)
         => Ok(await _appRepo.GetAsync(id).ToResponseAsync());
 
-    [HttpDelete("{id}")]
+    [HttpDelete("{id}"), Authorize]
+    [IsAuthorized(["Admin", "User"])]
     public async Task<IActionResult> Delete(int id)
         => Ok(await _appRepo.DeleteAsync(id).ToResponseAsync(message: ResponseMessages.USER_DELETED));
 }
