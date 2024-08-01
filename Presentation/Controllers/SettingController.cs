a7e18add2fdaf46ea84739bb22b6f279c28fbdd5
S.M.Owais <Hafizh@lifejourney.ai>
2024-08-01 08:03:03 +0500

    Update SettingController.cs

--- Presentation/Controllers/SettingController.cs
+++ Presentation/Controllers/SettingController.cs
@@ -13,17 +13,17 @@ namespace Presentation.Controllers
     {
         [HttpPut("{appId:int}"), Authorize]
         [IsAuthorized(["Admin", "User"])]
-        public async Task<IActionResult> Put(int appId,UpdateSettingDTO request)
-      => Ok(await _settingRepo.UpdateAsync(appId, request).ToResponseAsync(message: ResponseMessages.APP_UPDATED));
+        public async Task<IActionResult> Put(int appId, UpdateSettingDTO request)
+            => Ok(await _settingRepo.UpdateAsync(appId, request).ToResponseAsync(message: ResponseMessages.APP_UPDATED));
 
         [HttpGet("{id}")]
-        //[IsAuthorized(["Admin", "User"])]
-        public async Task<IActionResult> Put(int id)
-           => Ok(await _settingRepo.GetAsync(id).ToResponseAsync());
+        [IsAuthorized(["Admin", "User"])]
+        public async Task<IActionResult> Get(int id)
+            => Ok(await _settingRepo.GetAsync(id).ToResponseAsync());
 
         [HttpPut("{appId:int},{IsLive:bool}"), Authorize]
         [IsAuthorized(["Admin", "User"])]
         public async Task<IActionResult> Put(int appId, bool IsLive)
-      => Ok(await _settingRepo.UpdateAppStatusAsync(appId, IsLive).ToResponseAsync(message: ResponseMessages.APP_UPDATED));
+            => Ok(await _settingRepo.UpdateAppStatusAsync(appId, IsLive).ToResponseAsync(message: ResponseMessages.APP_UPDATED));
     }
 }
